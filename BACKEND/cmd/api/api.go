70053fe55c557590c94f8be83122c1909a614ad7
Yanlex <takashi21@yandex.ru>
2025-03-31 14:26:39 +0300

    фиксим бекенд и фронт

--- BACKEND/cmd/api/api.go
+++ BACKEND/cmd/api/api.go
@@ -93,7 +93,7 @@ func getData(w http.ResponseWriter, r *http.Request) {
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://wow-guild-front-nginx")
+		w.Header().Set("Access-Control-Allow-Origin", "https://kvd.sanyadev.ru")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
